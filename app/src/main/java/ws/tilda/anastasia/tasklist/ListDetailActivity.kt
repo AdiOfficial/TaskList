f9f388681890626d3fa0301feb1c3805d2e5804e
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-11-17 23:22:55 +0200

    Added FAB to add tasks in the activity layout and in the activity code

--- app/src/main/java/ws/tilda/anastasia/tasklist/ListDetailActivity.kt
+++ app/src/main/java/ws/tilda/anastasia/tasklist/ListDetailActivity.kt
@@ -1,6 +1,7 @@
 package ws.tilda.anastasia.tasklist
 
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -8,6 +9,7 @@ import android.support.v7.widget.RecyclerView
 class ListDetailActivity : AppCompatActivity() {
     lateinit var list: TaskList
     lateinit var listItemsRecyclerView: RecyclerView
+    lateinit var addTaskButton: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +20,13 @@ class ListDetailActivity : AppCompatActivity() {
         listItemsRecyclerView = findViewById(R.id.list_items_recyclerview)
         listItemsRecyclerView.layoutManager = LinearLayoutManager(this)
         listItemsRecyclerView.adapter = ListItemsRecyclerViewAdapter(list)
+        addTaskButton = findViewById(R.id.add_task_button)
+        addTaskButton.setOnClickListener {
+            showCreateTaskDialog()
+        }
+    }
+
+    private fun showCreateTaskDialog() {
+
     }
 }
