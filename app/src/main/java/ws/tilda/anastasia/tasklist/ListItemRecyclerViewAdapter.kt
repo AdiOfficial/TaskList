23ee78d4f8f183a882df597d9aa3a304c8c2eba5
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-11-17 23:08:53 +0200

    Created a layout to viewholder for TaskList items, implemented 2 methods in the new Adapter class

--- app/src/main/java/ws/tilda/anastasia/tasklist/ListItemRecyclerViewAdapter.kt
+++ app/src/main/java/ws/tilda/anastasia/tasklist/ListItemRecyclerViewAdapter.kt
@@ -1,15 +1,17 @@
 package ws.tilda.anastasia.tasklist
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.ViewGroup
 
-class ListItemRecyclerViewAdapter(var list: TaskList) : RecyclerView.Adapter<ListItemViewHolder> {
+class ListItemRecyclerViewAdapter(var list: TaskList) : RecyclerView.Adapter<ListItemViewHolder>() {
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ListItemViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = LayoutInflater.from(p0?.context).inflate(R.layout.task_view_holder, p0, false)
+        return ListItemViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return list.tasks.size
     }
 
     override fun onBindViewHolder(p0: ListItemViewHolder, p1: Int) {
