3010d36613e1d2d68609689b1a8e7c387958512e
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-11-17 18:27:39 +0200

    -Added a layout for the item in the list - Created viewholder in the Adapter's class corresponding function

--- app/src/main/java/ws/tilda/anastasia/tasklist/ListSelectionRecyclerViewAdapter.kt
+++ app/src/main/java/ws/tilda/anastasia/tasklist/ListSelectionRecyclerViewAdapter.kt
@@ -1,13 +1,15 @@
 package ws.tilda.anastasia.tasklist
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.ViewGroup
 
-class ListSelectionRecyclerViewAdapter(): RecyclerView.Adapter<ListSelectionViewHolder>() {
-   val listOfTitles = arrayOf("Shoppint List", "Chores", "Android Tutorials")
+class ListSelectionRecyclerViewAdapter() : RecyclerView.Adapter<ListSelectionViewHolder>() {
+    val listOfTitles = arrayOf("Shoppint List", "Chores", "Android Tutorials")
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ListSelectionViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = LayoutInflater.from(p0?.context).inflate(R.layout.list_selection_view_holder, p0, false)
+        return ListSelectionViewHolder(view)
     }
 
     override fun getItemCount(): Int {
