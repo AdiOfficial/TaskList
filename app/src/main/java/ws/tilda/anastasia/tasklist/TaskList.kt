8f978932298e465b67fc43298be79e04d532fa6b
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-11-17 22:17:55 +0200

    Created a parcelable interface in order to pass the list into bundle with the intent

--- app/src/main/java/ws/tilda/anastasia/tasklist/TaskList.kt
+++ app/src/main/java/ws/tilda/anastasia/tasklist/TaskList.kt
@@ -1,6 +1,33 @@
 package ws.tilda.anastasia.tasklist
 
-class TaskList(val name: String, val tasks: ArrayList<String> = ArrayList<String>()) {
+import android.os.Parcel
+import android.os.Parcelable
+
+class TaskList(val name: String, val tasks: ArrayList<String> = ArrayList<String>()): Parcelable {
+
+    /*
+Adding secondary constructor. The TaskList object will be created from the passed in parcel. This word is used to pass
+data read from the parcel to the primary constructor
+*/
+    constructor(source: Parcel) : this(source.readString(), source.createStringArrayList())
+
+    override fun describeContents(): Int = 0
+
+    // This function is used when creating parcelable from the TasList
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(name)
+        dest.writeStringList(tasks)
+    }
+
+    companion object CREATOR : Parcelable.Creator<TaskList> {
+        override fun createFromParcel(parcel: Parcel): TaskList {
+            return TaskList(parcel)
+        }
+
+        override fun newArray(size: Int): Array<TaskList?> {
+            return arrayOfNulls(size)
+        }
+    }
 
 
 }
